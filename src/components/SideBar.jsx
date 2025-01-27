bcba973fa4ae8976f05916cc3dd3ca4bb06dee22
pookey1104 <pookey1104@naver.com>
2025-01-27 17:08:05 +0900

    ✨ Feat: 각 Header 적용 및 SideBar 추가 (#2)

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "@styles/SideBarStyle";
+
+function SideBar() {
+  return (
+    <>
+      <S.SideBar>사이드바 컴포넌트</S.SideBar>
+    </>
+  );
+}
+
+export default SideBar;
