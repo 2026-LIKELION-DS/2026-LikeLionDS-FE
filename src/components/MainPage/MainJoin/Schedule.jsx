3bf514a05e496d4b39a902e9ecfdafaed94acc6f
jinhyo <kjhyo0220@gmail.com>
2025-01-30 04:00:08 +0900

    ✨ Feat: 모집 일정 스크롤 기능 (#5)
    스크롤 내리면 원 색 바뀌도록

--- src/components/MainPage/MainJoin/Schedule.jsx
+++ src/components/MainPage/MainJoin/Schedule.jsx
@@ -2,6 +2,8 @@ import JoinRectangle from "./JoinRectangle";
 import palette from "@lib/colorPalette";
 import * as M from "@styles/ScheduleStyle";
 
+import { useEffect, useRef, useState } from "react";
+
 const JoinScheduleRectangle = ({ children }) => {
   return (
     <JoinRectangle
@@ -11,17 +13,39 @@ const JoinScheduleRectangle = ({ children }) => {
       bgr={palette.boldBlack.ex7}
       color={palette.style.white}
       fontWeight={500}
-      fontSize={"16px"}>
+      fontSize={"16px"}
+      letterSpacing={"-0.48px"}>
       {children}
     </JoinRectangle>
   );
 };
 
-const Schedule = ({ date, text, children }) => {
+const Schedule = ({ date, text, children, className }) => {
+  const [isVisible, setIsVisible] = useState(false); // 화면에 보이는지 여부
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting); // 화면에 보이면 true
+      },
+      {
+        rootMargin: "-48% 0px -48% 0px",
+      },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
   return (
     <>
-      <M.ScheduleWrapper>
-        <M.Circle />
+      <M.ScheduleWrapper className={className} ref={ref}>
+        <M.Circle isVisible={isVisible} />
         <M.DateText>{date}</M.DateText>
         <JoinScheduleRectangle>{text}</JoinScheduleRectangle>
       </M.ScheduleWrapper>
--- src/styles/ScheduleStyle.js
+++ src/styles/ScheduleStyle.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import palette from "@lib/colorPalette";
 
 export const ScheduleWrapper = styled.div`
-  width: 342px;
+  width: 335px;
   display: flex;
 
   flex-direction: row;
@@ -15,8 +15,10 @@ export const Circle = styled.div`
   height: 10px;
 
   border-radius: 50%;
-
-  background-color: ${palette.boldBlack.ex8};
+  background-color: ${(props) => (props.isVisible ? palette.duksungBurgundy.ex5Primary : palette.boldBlack.ex8)};
+  transition:
+    background-color 0.3s ease,
+    opacity 0.3s ease;
 `;
 
 export const DateText = styled.p`
@@ -24,6 +26,10 @@ export const DateText = styled.p`
   font-size: 16px;
   font-style: normal;
   font-weight: 500;
-  line-height: 130%; /* 20.8px */
+  line-height: 130%;
   letter-spacing: -0.48px;
+
+  width: 96px;
+
+  margin-left: -17.5px;
 `;
