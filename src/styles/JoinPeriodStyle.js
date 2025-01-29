10657cd5857ec7cf7081de6a475920377b5223d3
jinhyo <kjhyo0220@gmail.com>
2025-01-29 23:09:50 +0900

    💄 Design: 활동 기간 디자인 수정 (#5)
    margin 수정

--- src/styles/JoinPeriodStyle.js
+++ src/styles/JoinPeriodStyle.js
@@ -3,15 +3,17 @@ import palette from "@lib/colorPalette";
 
 export const TextWrapper = styled.div`
   width: 342px;
+
+  margin-top: 16px;
 `;
 
 export const Text = styled.p`
   color: ${palette.boldBlack.ex2};
-  /* Light_16 */
+
   font-family: Pretendard;
   font-size: 16px;
   font-style: normal;
   font-weight: 300;
-  line-height: 130%; /* 20.8px */
+  line-height: 130%;
   letter-spacing: -0.48px;
 `;
