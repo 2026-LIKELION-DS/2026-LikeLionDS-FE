5cd30d1c01f81417380f55d24a53211de9bcd1f2
mzxxzysy <jsyjj21@gmail.com>
2025-02-02 22:56:14 +0900

    🔒️ Lock: 환경변수 설정
    어드민 메뉴 페이지 환경변수 추가 설정

--- src/routes/AdminMenu.jsx
+++ src/routes/AdminMenu.jsx
@@ -5,13 +5,15 @@ import Header from "@components/Header/HeaderAdmin";
 
 function AdminMenu() {
   const navigate = useNavigate();
+  const ADMIN_QNA = import.meta.env.VITE_ADMIN_QNA;
+  const ADMIN_NOTI = import.meta.env.VITE_ADMIN_NOTI;
+
   return (
     <>
       <Header title="운영진이시네요" />
       <A.AdminMenu>
-        {" "}
-        <A.Menu onClick={() => navigate("/admin/notice/new")}>Q&A 답변하기</A.Menu>
-        <A.Menu onClick={() => navigate("/admin/notice/:id/edit")}>공지사항 글쓰기</A.Menu>
+        <A.Menu onClick={() => navigate(`${ADMIN_QNA}`)}>Q&A 답변하기</A.Menu>
+        <A.Menu onClick={() => navigate(`${ADMIN_NOTI}`)}>공지사항 글쓰기</A.Menu>
       </A.AdminMenu>
     </>
   );
