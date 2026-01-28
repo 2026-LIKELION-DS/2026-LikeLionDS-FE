ed2c6dfa2edefd1a0901e9ff5897340528ded48c
aney0714 <gimhh770@gmail.com>
2026-01-28 15:35:42 +0900

    [Refactor]버튼 연결,스타일 수정

--- src/routes/NoExistApplication.jsx
+++ src/routes/NoExistApplication.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import * as SS from "@styles/SubmittedPageStyle";
-import * as S from "@styles/SubmittedApplication";
+import * as S from "@styles/SubmittedApplicationStyle";
 
 import kakao from "@/assets/icons/icon_kakaotalk.svg"; 
 
@@ -12,7 +12,29 @@ import Footer from "@components/Footer";
 
 
 function NoExistApplication({ title, messages = [], highlightText, showKakaoLink = false, onKakaoClick }) {
+  const navigate = useNavigate();
+  // 마감일 설정 (2026년 2월 19일 18:00)
+  const deadline = new Date(2026, 1, 19, 18, 0, 0);
 
+ // 현재 시간이 마감 기한을 넘었는지 확인하는 함수
+ const checkDeadline = () => new Date() >= deadline;
+
+ // 마감 여부 상태 관리
+ const [isDeadlinePassed, setIsDeadlinePassed] = useState(checkDeadline());
+
+ useEffect(() => {
+  // 실시간 상태 업데이트를 위한 타이머 (1분 간격)
+  const timer = setInterval(() => {
+    setIsDeadlinePassed(checkDeadline());
+  }, 60000);
+  return () => clearInterval(timer);
+}, []);
+
+const handleClick = () => {
+  if (!isDeadlinePassed) {
+    navigate("/WriteInformation");
+  }
+};
 
   return (
     <>
@@ -30,7 +52,7 @@ function NoExistApplication({ title, messages = [], highlightText, showKakaoLink
                     해당하는 이름, 전화번호, 메일로<br/>제출된 지원서가 없습니다.
                 </S.Text>
                 <S.Text>
-                    관련 문의사항은 카카오톡 오픈채팅으로 부탁드립니다.
+                    관련 문의사항은 <br/>카카오톡 오픈채팅으로 부탁드립니다.
                 </S.Text>
                 <S.KakaoLink onClick={() => window.open("https://open.kakao.com/...", "_blank")}>
   <S.KakaoIMG src={kakao} alt="카카오톡 로고" />
@@ -38,8 +60,10 @@ function NoExistApplication({ title, messages = [], highlightText, showKakaoLink
 </S.KakaoLink>
               
               </S.Card>
-              <S.Submit_Button>지원서 제출하러 가기</S.Submit_Button>
-              {/* 링크 아직 안 걸어둠 */}
+              <S.DisabledButton $isDeadlinePassed={isDeadlinePassed} onClick={handleClick}>
+              {isDeadlinePassed ? "지금은 모집 기간이 아니에요" : "지원서 제출하러 가기"}
+              </S.DisabledButton>
+           
             </S.Wrapper>
       </SS.Space>
 
