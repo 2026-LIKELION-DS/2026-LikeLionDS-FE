69b909ece807bf91d6873d65beec3d764444ee36
허윤아 <aquahua@gmail.com>
2026-01-30 14:19:07 +0900

    ♻ Refactor: UpIcon 위치 수정 (#57)

--- src/styles/WriteConfirmStyle.js
+++ src/styles/WriteConfirmStyle.js
@@ -127,7 +127,7 @@ export const Fixed = styled.div`
 
   position: ${({ $withNext }) => ($withNext ? "sticky" : "fixed")};
   bottom: ${({ $withNext }) => ($withNext ? "120px" : "24px")};
-  right: ${({ $withNext }) => ($withNext ? "auto" : "24px")};
+  right: ${({ $withNext }) => ($withNext ? "auto" : "calc(50% - 178px)")};
   margin-left: ${({ $withNext }) => ($withNext ? "auto" : "0")};
 
   z-index: 1000;
