7f6293826c01ca040c9f89dd0f131249d007ac87
허윤아 <aquahua@gmail.com>
2026-02-03 13:58:57 +0900

    ♻ Refactor: SideBar 날짜 수정 (#103)

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -76,7 +76,7 @@ function SideBar({
   // 최종 합격자 조회 가능 기간 (QA용)
   const isFinalDateRange = () => {
     const today = new Date();
-    const startDate = new Date(2026, 2, 1, 12, 0, 0);
+    const startDate = new Date(2026, 2, 7, 12, 0, 0);
     // // 최종합격 UI 보고싶을 시
     // const startDate = new Date(2026, 1, 1, 12, 0, 0);
     const endDate = new Date(2026, 2, 11, 0, 0, 0);
